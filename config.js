be25be1bbced7a9d60d9ccc0695920881d7cafc3
rolylove305 <rolylove305@gmail.com>
2026-07-05 21:01:29 -0400

    Point TMS to dedicated Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ZAP_TMS_CONFIG={
-  url:"https://gfuazkuohrragkthtumz.supabase.co",
-  token:["sb_pub","lishable_HJ","Nw6TpNJ0YwTo4rEME6zg","_Gbp1xtGU"].join("")
+  url:"https://nhttbtvjcwtkecdlxpzk.supabase.co",
+  token:["sb_pub","lishable_IfzdN","ltSaCOHh8703TWt0w","_k4KDxJcT"].join("")
 };
