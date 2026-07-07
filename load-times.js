98a1828e2d96061592f7f1127d25d9e14cec9ceb
rolylove305 <rolylove305@gmail.com>
2026-07-07 12:50:33 -0400

    Load strict folder guard

--- load-times.js
+++ load-times.js
@@ -5,6 +5,7 @@ function run(){
   load('tmsDocsStable','tms-docs-stable.js?v=5200');
   load('storageUploadHelper','storage-upload.js?v=5600');
   load('cardStabilizerHelper','card-stabilizer.js?v=5600');
+  load('folderGuardHelper','folder-guard.js?v=5700');
 }
 setTimeout(run,500);
 setInterval(run,5000);
